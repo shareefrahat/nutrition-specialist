53d695bce96de98e0cd9a9f9a1771914af4f5a71
ShareefRahat <imr4h47@gmail.com>
2022-04-17 13:06:43 +0600

    footer ui added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+
   return (
-    <footer>
-      <p>&copy; Copyright 2022 | Nutrition Specialist</p>
-    </footer>
+    <>
+      <footer className="bg-green-900 text-white p-5 text-md lg:text-xl font-serif mt-40">
+        <p>&copy; Copyright {year} | Nutrition Specialist</p>
+      </footer>
+    </>
   );
 };
 
