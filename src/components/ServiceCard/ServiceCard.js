9e22fdb38a985f5e0032215d9ccc5a41a95916fd
ShareefRahat <imr4h47@gmail.com>
2022-04-17 20:05:25 +0600

    services card and navigation added

--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
-  const { name } = service;
+  const { name, img, description } = service;
+  const navigate = useNavigate("");
+
   return (
     <>
-      <div>
-        <p>{name}</p>
+      <div
+        onClick={() => navigate("/checkout")}
+        className="shadow-md bg-white mx-auto w-fit rounded-xl p-4 border-2
+      transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300
+      "
+      >
+        <div>
+          <img
+            className="w-[200px] md:w-[300px] mx-auto rounded-md"
+            src={img}
+            alt=""
+          />
+        </div>
+        <div>
+          <p className="text-lg md:text-xl font-semibold py-3">{name}</p>
+        </div>
+        <div>
+          <p className="w-[200px] md:w-[300px] mx-auto text-justify text-lg">
+            {description}
+          </p>
+        </div>
       </div>
     </>
   );
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -16,7 +16,7 @@ const Services = () => {
         {!services ? (
           <div>loading</div>
         ) : (
-          <div>
+          <div className="grid grid-cols-1 lg:grid-cols-3 mt-10 gap-10">
             {services.map((service) => (
               <ServiceCard key={service.id} service={service}></ServiceCard>
             ))}
